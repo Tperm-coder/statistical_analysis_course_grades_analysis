b27a350fa5de8c16aad2c3e6aa06835e614e1ddc
youssif gamal <77980007+Tperm-coder@users.noreply.github.com>
2022-01-04 16:57:56 +0200

    Update stat_grades_analysis.py

--- stat_grades_analysis.py
+++ stat_grades_analysis.py
@@ -4,6 +4,27 @@ from matplotlib import pyplot as plt
 plt.style.use('bmh')
 dec_to_bin = [[0,0],[0,1],[1,0],[1,1]]
 
+def get_mean(data):
+    return sum(data) / len(data)
+
+def get_median(data):
+    data.sort()
+    if len(data) % 2 == 0:
+        median = (data[int(len(data)/2)] + data[int(len(data)/2) - 1]) / 2
+    else:
+        median = data[int(len(data)/2)]
+    return median
+
+def get_mode(data):
+    data_frequency = collections.Counter(data)
+    data_frequency = collections.OrderedDict(sorted(data_frequency.items()))
+    return max(data_frequency, key=data_frequency.get)
+
+def get_standard_deviation(data):
+    mean = get_mean(data)
+    return (sum([(x - mean)**2 for x in data]) / len(data))**0.5
+
+
 def build_graph(data_lists, isKDE = False):
 
     fig, axs = plt.subplots(2,2)
@@ -26,8 +47,10 @@ def build_graph(data_lists, isKDE = False):
         axs[dec_to_bin[index][0],dec_to_bin[index][1]].title.set_text(graph_title)
 
         if isKDE :
-            sns.histplot(x = data , y = frequency , ax=axs[dec_to_bin[index][0],dec_to_bin[index][1]])
+            import seaborn as sns
+
             sns.set_theme(style="darkgrid")
+            sns.histplot(x = data , y = frequency , ax=axs[dec_to_bin[index][0],dec_to_bin[index][1]])
         else :
             axs[dec_to_bin[index][0],dec_to_bin[index][1]].bar(data, frequency, color ='darkcyan')
 
@@ -56,11 +79,28 @@ for i in range(1,len(data)) : #starting from index 1 to skip headers
     final_grades.append(float(data[i][6]))
 
 
-
-build_graph([[final_grades,"Total Grades"],
+data_lists = [[final_grades,"Total Grades"],
 [midterm_grades,"Midterm Grades"],
 [quiz_one_grades,"Quiz One Grades"],
-[quiz_two_grades,"Quiz Two Grades"]])
+[quiz_two_grades,"Quiz Two Grades"]]
+
+
+print("\n\n")
+for i in data_lists:
+
+    print(i[1] + " \nMean: " 
+    + str(get_mean(i[0])) 
+    + " \nMedian: " 
+    + str(get_median(i[0])) 
+    + " \nMode: " 
+    + str(get_mode(i[0])) 
+    + " \nStandard Deviation: " 
+    + str(get_standard_deviation(i[0]))) 
+
+    print("\n")
+
+build_graph(data_lists)  #,isKDE = True)
 
 plt.subplots_adjust(left=0.1 , bottom=0.1 , right=0.9 , top=0.9 , wspace=0.2 , hspace=0.2)
 plt.show()
+
